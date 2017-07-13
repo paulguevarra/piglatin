0cd312f602878d02a348f2faa9dbe45a5afd0762
Guest User <Guest@Epicodus-9E.local>
2017-07-13 08:13:50 -0700

    fix bug of loop continueing to second vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 var vowels = ["a", "e", "i", "o", "u"];
 var consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","q", "r","s","t","v","w","x","y","z"]
 var emptyArray = [];
-var work1 = [];
+var total;
 var work2 = [];
 
 //var working = userInput1.splice(0,1);
@@ -23,25 +23,27 @@ $(document).ready(function(){
     for(var i = 0; i < userInput1.length; i++) {
       if(vowels.includes(userInput1.charAt(0))) {
         emptyArray += userInput1 + "way";
-      } else if (vowels.includes(userInput1.charAt(i))){
-        if (userInput1.charAt(i) === "q" && userInput1.charAt(i+1) === "u") {
-alert("check")
-          // var newInput = userInput1.slice(0,i);
-          // var endInput  = userInput1.slice(i++, userInput1.length);
-          // var total  = endInput + newInput + 'ay';
-          // alert(total);
-          // return ;
-        } else if (vowels.includes(userInput1.charAt(i))) {
-          var newInput = userInput1.slice(0,i);
-          var endInput  = userInput1.slice(i, userInput1.length);
-          var total  = endInput + newInput + 'ay';
-          alert(total);
-          return ;
+      // } else if (vowels.includes(userInput1.charAt(i))){
+      //   if (userInput1.charAt(i) === "q" && userInput1.charAt(i++) === "u") {
+      //
+      //     // var newInput = userInput1.slice(0,i);
+      //     // var endInput  = userInput1.slice(i++, userInput1.length);
+      //     // var total  = endInput + newInput + 'ay';
+      //     // alert(total);
+      //     // return ;
+      } else if (vowels.includes(userInput1.charAt(i))) {
+        var newInput = userInput1.slice(0,i);
+        var endInput  = userInput1.slice(i, userInput1.length);
+        total  = endInput + newInput + 'ay';
+        alert(total);
+        // return total;
+        console.log(total);
         // vowels.find(userInput1)
 //alert ("check");
-        }
+        break;
+
       }
     }
-    $("#result").text(result);
+    $("#result").text(total);
   });
 });
